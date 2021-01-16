74d9725ed1c7876ec0d6f0f78f02899ee0ff9a39
Bryan Wong <bryanjwong@g.ucla.edu>
2021-01-15 23:19:22 -0800

    implemented placeholder graph

--- docs/web/src/Components/Graph.js
+++ docs/web/src/Components/Graph.js
@@ -1,13 +1,40 @@
 import { makeStyles } from "@material-ui/core";
-import React from "react";
+import React, { useEffect } from "react";
+import Chart from "chart.js"
 
 const useStyles = makeStyles({
 });
 
 function Graph() {
     const classes = useStyles();
+    const chartRef = React.createRef();
+    
+    useEffect(() => {
+        const myChartRef = chartRef.current.getContext("2d");
+        new Chart(myChartRef, {
+            type: "line",
+            data: {
+                //Bring in data
+                labels: ["Jan", "Feb", "March"],
+                datasets: [
+                    {
+                        label: "Sales",
+                        data: [86, 67, 91],
+                    }
+                ]
+            },
+            options: {
+                //Customize chart options
+            }
+        });
+    });
 
-    return (<div>I am a graph</div>)
+    return (<div>
+        <canvas
+                    id="myChart"
+                    ref={chartRef}
+                />
+    </div>)
 }
 
 export default Graph;
