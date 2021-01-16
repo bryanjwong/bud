eaa1ef386f35ec0cf4a192c818e5f1493945e6ec
Bryan Wong <bryanjwong@g.ucla.edu>
2021-01-16 13:18:35 -0800

    moved api calls to HomePage.js

--- docs/web/src/Components/Dashboard.js
+++ docs/web/src/Components/Dashboard.js
@@ -40,77 +40,13 @@ const useStyles = makeStyles({
     }
 });
 
-const proxyurl = "https://cors-anywhere.herokuapp.com/";
-
-function Dashboard() {
+function Dashboard(props) {
     const classes = useStyles();
-    const [tSoilMoisture, setTSoilMoisture] = useState(40);
-    const [tHumidity, setTHumidity] = useState(40);
-    const [tTemp, setTTemp] = useState(50);
-    const [tLight, setTLight] = useState(10);
-    const [soilMoistureData, setSoilMoistureData] = useState([]);
-    const [humidityData, setHumidityData] = useState([]);
-    const [tempData, setTempData] = useState([]);
-    const [lightData, setLightData] = useState([]);
-
-    async function createNewPlant(name) {
-        const searchResp = await axios
-            .get(proxyurl + "https://trefle.io/api/v1/species/search?token=R8xZcZH6j9PoyOTqoGc_Pndhdvx6nM1aD7FGHYBQc2M", {
-                params: {
-                    q: name
-                }
-            });
-        if (searchResp.data.data.length == 0) {
-            console.log("No results found.");
-            return;
-        }
-        const plantId = searchResp.data.data[0].id;
-    
-        const plantResp = await axios 
-            .get(proxyurl + `https://trefle.io/api/v1/species/${plantId}?token=R8xZcZH6j9PoyOTqoGc_Pndhdvx6nM1aD7FGHYBQc2M`);
-        const plantInfo = plantResp.data.data;
-        const growthInfo = plantInfo.growth;
-
-        if (growthInfo.soil_humidity) setTSoilMoisture(growthInfo.soil_humidity);
-        if (growthInfo.atmospheric_humidity) setTHumidity(growthInfo.atmospheric_humidity * 10);
-        if (growthInfo.minimum_temperature) setTTemp(growthInfo.minimum_temperature.deg_f);
-        if (growthInfo.light) setTLight(growthInfo.light * 4);
-    };
-
-    function loadFirebaseData() {
-        let soilMoisture = [];
-        let humidity = [];
-        let temp = [];
-        let light = [];
-        var query = db.ref("active-plant/data");
-        query.once("value")
-        .then(function(snapshot) {
-            snapshot.forEach(function(childSnapshot) {
-                // key is ISO timestamp
-                var key = childSnapshot.key;
-
-                var data = childSnapshot.val().split(",");
-                soilMoisture.push({t:key, y:parseInt(data[0])});
-                humidity.push({t:key, y:parseInt(data[1])});
-                temp.push({t:key, y:parseInt(data[2])});
-                light.push({t:key, y:parseInt(data[3])});
-            });
-            setSoilMoistureData(soilMoisture);
-            setHumidityData(humidity);
-            setTempData(temp);
-            setLightData(light);
-        });
-    }
-
-    useEffect(() => {
-        createNewPlant('Sunflower');
-        loadFirebaseData();   
-    }, []);
-
-    var soilMoistureStr = (soilMoistureData.length > 0) ? soilMoistureData.slice(-1)[0].y : "?";
-    var humidityStr = (humidityData.length > 0) ? humidityData.slice(-1)[0].y : "?";
-    var tempStr = (tempData.length > 0) ? tempData.slice(-1)[0].y : "?";
-    var lightStr = (lightData.length > 0) ? lightData.slice(-1)[0].y : "?";
+    console.log(props)
+    var soilMoistureStr = (props.soilMoistureData.length > 0) ? props.soilMoistureData.slice(-1)[0].y : "?";
+    var humidityStr = (props.humidityData.length > 0) ? props.humidityData.slice(-1)[0].y : "?";
+    var tempStr = (props.tempData.length > 0) ? props.tempData.slice(-1)[0].y : "?";
+    var lightStr = (props.lightData.length > 0) ? props.lightData.slice(-1)[0].y : "?";
     return (
     <Grid className={classes.root}>
         <Grid container>
@@ -122,13 +58,13 @@ function Dashboard() {
             </Grid>
         </Grid>
         <p className={classes.labels}>Your plant's progress</p>
-        <Graph soilMoistureData={soilMoistureData} humidityData={humidityData} tempData={tempData} lightData={lightData}/>
+        <Graph soilMoistureData={props.soilMoistureData} humidityData={props.humidityData} tempData={props.tempData} lightData={props.lightData}/>
         <p className={classes.labels}>Helpful stats</p>
         <Grid container spacing={2}>
-            <Grid item xs={3}><Metric metric="Soil Moisture" actual={soilMoistureStr} target={tSoilMoisture}/></Grid>
-            <Grid item xs={3}><Metric metric="Humidity" actual={humidityStr} target={tHumidity}/></Grid>
-            <Grid item xs={3}><Metric metric="Temperature" actual={tempStr} target={tTemp}/></Grid>
-            <Grid item xs={3}><Metric metric="Light" actual={lightStr} target={tLight}/></Grid>
+            <Grid item xs={3}><Metric metric="Soil Moisture" actual={soilMoistureStr} target={props.tSoilMoisture}/></Grid>
+            <Grid item xs={3}><Metric metric="Humidity" actual={humidityStr} target={props.tHumidity}/></Grid>
+            <Grid item xs={3}><Metric metric="Temperature" actual={tempStr} target={props.tTemp}/></Grid>
+            <Grid item xs={3}><Metric metric="Light" actual={lightStr} target={props.tLight}/></Grid>
         </Grid>
     </Grid>
     )
--- docs/web/src/Components/HomePage.js
+++ docs/web/src/Components/HomePage.js
@@ -1,15 +1,82 @@
 import { makeStyles } from "@material-ui/core";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Grid from "@material-ui/core/Grid";
 import ActivityLog from "./ActivityLog";
 import Dashboard from "./Dashboard";
+import axios from "axios";
+import {db} from "../Services/Firebase"
 
 const useStyles = makeStyles(
     
 );
 
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
+
 function HomePage() {
     const classes = useStyles();
+
+    const [tSoilMoisture, setTSoilMoisture] = useState(40);
+    const [tHumidity, setTHumidity] = useState(40);
+    const [tTemp, setTTemp] = useState(50);
+    const [tLight, setTLight] = useState(10);
+    const [soilMoistureData, setSoilMoistureData] = useState([]);
+    const [humidityData, setHumidityData] = useState([]);
+    const [tempData, setTempData] = useState([]);
+    const [lightData, setLightData] = useState([]);
+
+    async function createNewPlant(name) {
+        const searchResp = await axios
+            .get(proxyurl + "https://trefle.io/api/v1/species/search?token=R8xZcZH6j9PoyOTqoGc_Pndhdvx6nM1aD7FGHYBQc2M", {
+                params: {
+                    q: name
+                }
+            });
+        if (searchResp.data.data.length == 0) {
+            console.log("No results found.");
+            return;
+        }
+        const plantId = searchResp.data.data[0].id;
+    
+        const plantResp = await axios 
+            .get(proxyurl + `https://trefle.io/api/v1/species/${plantId}?token=R8xZcZH6j9PoyOTqoGc_Pndhdvx6nM1aD7FGHYBQc2M`);
+        const plantInfo = plantResp.data.data;
+        const growthInfo = plantInfo.growth;
+
+        if (growthInfo.soil_humidity) setTSoilMoisture(growthInfo.soil_humidity);
+        if (growthInfo.atmospheric_humidity) setTHumidity(growthInfo.atmospheric_humidity * 10);
+        if (growthInfo.minimum_temperature) setTTemp(growthInfo.minimum_temperature.deg_f);
+        if (growthInfo.light) setTLight(growthInfo.light * 4);
+    };
+
+    function loadFirebaseData() {
+        let soilMoisture = [];
+        let humidity = [];
+        let temp = [];
+        let light = [];
+        var query = db.ref("active-plant/data");
+        query.once("value")
+        .then(function(snapshot) {
+            snapshot.forEach(function(childSnapshot) {
+                // key is ISO timestamp
+                var key = childSnapshot.key;
+
+                var data = childSnapshot.val().split(",");
+                soilMoisture.push({t:key, y:parseInt(data[0])});
+                humidity.push({t:key, y:parseInt(data[1])});
+                temp.push({t:key, y:parseInt(data[2])});
+                light.push({t:key, y:parseInt(data[3])});
+            });
+            setSoilMoistureData(soilMoisture);
+            setHumidityData(humidity);
+            setTempData(temp);
+            setLightData(light);
+        });
+    }
+
+    useEffect(() => {
+        createNewPlant('Sunflower');
+        loadFirebaseData();   
+    }, []);
 
     return (
         <Grid container>
@@ -17,7 +84,8 @@ function HomePage() {
                 <ActivityLog />
             </Grid>
             <Grid item xs={12} lg={8}>
-                <Dashboard />
+                <Dashboard tSoilMoisture={tSoilMoisture} tHumidity={tHumidity} tTemp={tTemp} tLight={tLight}
+                           soilMoistureData={soilMoistureData} humidityData={humidityData} tempData={tempData} lightData={lightData}/>
             </Grid>
         </Grid>
     )
