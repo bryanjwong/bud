cc17db0135540cbd72a5e0e32c401ed15140a78b
Kathy Daniels <danielskathyd@gmail.com>
2021-01-16 12:16:49 -0800

    fix graph

--- docs/web/src/Components/Graph.js
+++ docs/web/src/Components/Graph.js
@@ -1,7 +1,8 @@
 import { makeStyles } from "@material-ui/core";
 import React, { useEffect } from "react";
 import Grid from "@material-ui/core/Grid";
-import Chart from "chart.js"
+import Chart from "chart.js";
+import {db} from "../Services/Firebase"
 
 //--Chart Style Options--//
 Chart.defaults.global.defaultFontFamily = "Poppins"
@@ -15,74 +16,84 @@ const useStyles = makeStyles({
 function Graph() {
     const classes = useStyles();
     const chartRef = React.createRef();
+    let soilMoisture = [];
+    let humidity = [];
+    let temp = [];
+    let light = [];
     
     var lookbackPeriod = 60000; // 1 Hour
     const min_t = Date.parse("2021-01-15T22:26:57Z") - lookbackPeriod;
     const newDate = new Date(min_t);
     console.log(newDate);
+
     useEffect(() => {
         const myChartRef = chartRef.current.getContext("2d");
         const {width: graphWidth} = myChartRef.canvas;
         let gradientLine = myChartRef.createLinearGradient(0, 0, graphWidth * 2, 0);
         gradientLine.addColorStop(0, "#60BCB9");
         gradientLine.addColorStop(1, "#7ACEFA");
-        new Chart(myChartRef, {
-            type: "line",
-            data: {
-                //Bring in data
-                // labels: [min_t, "2021-01-15T22:26:57Z"],
-                datasets: [
-                    {
-                        label: "Humidity",
-                        data: [{
-                            t: "2021-01-15T22:24:47",
-                            y: 43
-                        },
-                        {
-                            t: "2021-01-15T22:25:52",
-                            y: 51
-                        },
-                        {
-                            t: "2021-01-15T22:26:57",
-                            y: 46
-                        }],
-                        fill: false,
-                        borderColor: gradientLine,
-                    }
-                ]
-            },
-            options: {
-                //Customize chart options
-                responsive: true,
-                maintainAspectRatio: true,
-                scales: {
-                    xAxes: [{
-                        type: 'time',
-                        time: {
-                            unit: 'minute',
-                            tooltipFormat: "MMM D, h:mm a"
-                        },
-                        bounds: 'data',
-                        ticks: { display: true },
-                        gridLines: {
-                            display: false,
-                            drawBorder: true
+
+        var query = db.ref("active-plant/data");
+        query.once("value")
+        .then(function(snapshot) {
+            snapshot.forEach(function(childSnapshot) {
+                // key is ISO timestamp
+                var key = childSnapshot.key;
+
+                var data = childSnapshot.val().split(",");
+                soilMoisture.push({t:key, y:parseInt(data[0])});
+                humidity.push({t:key, y:parseInt(data[1])});
+                temp.push({t:key, y:parseInt(data[2])});
+                light.push({t:key, y:parseInt(data[3])});
+
+                new Chart(myChartRef, {
+                    type: "line",
+                    data: {
+                        //Bring in data
+                        // labels: [min_t, "2021-01-15T22:26:57Z"],
+                        datasets: [
+                            {
+                                label: "Humidity",
+                                data: humidity,
+                                fill: false,
+                                borderColor: gradientLine,
+                            }
+                        ]
+                    },
+                    options: {
+                        //Customize chart options
+                        responsive: true,
+                        maintainAspectRatio: true,
+                        scales: {
+                            xAxes: [{
+                                type: 'time',
+                                time: {
+                                    unit: 'minute',
+                                    tooltipFormat: "MMM D, h:mm a"
+                                },
+                                bounds: 'data',
+                                ticks: { display: true },
+                                gridLines: {
+                                    display: false,
+                                    drawBorder: true
+                                }
+                            }],
+                            yAxes: [{     
+                                ticks: { 
+                                    display: true,
+                                    suggestedMin: 30,
+                                    suggestedMax: 80, 
+                                },
+                                gridLines: {
+                                    display: true,
+                                    drawBorder: false
+                                },
+                            }]
                         }
-                    }],
-                    yAxes: [{     
-                        ticks: { 
-                            display: true,
-                            suggestedMin: 30,
-                            suggestedMax: 80, 
-                        },
-                        gridLines: {
-                            display: true,
-                            drawBorder: false
-                        },
-                    }]
-                }
-            }
-        }); 
+                    }
+                });
+            });
+        });
     });
 
     return (
