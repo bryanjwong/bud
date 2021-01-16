24a43f551592ab24f493acc67c0a2eea415c1cd2
Bryan Wong <bryanjwong@g.ucla.edu>
2021-01-16 01:14:23 -0800

    line graph formatting

--- docs/web/src/Components/Graph.js
+++ docs/web/src/Components/Graph.js
@@ -1,6 +1,13 @@
 import { makeStyles } from "@material-ui/core";
 import React, { useEffect } from "react";
+import Grid from "@material-ui/core/Grid";
 import Chart from "chart.js"
+
+//--Chart Style Options--//
+Chart.defaults.global.defaultFontFamily = "Poppins"
+Chart.defaults.global.legend.display = false;
+Chart.defaults.global.elements.line.tension = 0.5;
+//--Chart Style Options--//
 
 const useStyles = makeStyles({
 });
@@ -9,32 +16,87 @@ function Graph() {
     const classes = useStyles();
     const chartRef = React.createRef();
     
+    var lookbackPeriod = 60000; // 1 Hour
+    const min_t = Date.parse("2021-01-15T22:26:57Z") - lookbackPeriod;
+    const newDate = new Date(min_t);
+    console.log(newDate);
     useEffect(() => {
         const myChartRef = chartRef.current.getContext("2d");
+        const {width: graphWidth} = myChartRef.canvas;
+        let gradientLine = myChartRef.createLinearGradient(0, 0, graphWidth * 2, 0);
+        gradientLine.addColorStop(0, "#60BCB9");
+        gradientLine.addColorStop(1, "#7ACEFA");
         new Chart(myChartRef, {
             type: "line",
             data: {
                 //Bring in data
-                labels: ["Jan", "Feb", "March"],
+                // labels: [min_t, "2021-01-15T22:26:57Z"],
                 datasets: [
                     {
-                        label: "Sales",
-                        data: [86, 67, 91],
+                        label: "Humidity",
+                        data: [{
+                            t: "2021-01-15T22:24:47",
+                            y: 43
+                        },
+                        {
+                            t: "2021-01-15T22:25:52",
+                            y: 51
+                        },
+                        {
+                            t: "2021-01-15T22:26:57",
+                            y: 46
+                        }],
+                        fill: false,
+                        borderColor: gradientLine,
                     }
                 ]
             },
             options: {
                 //Customize chart options
+                responsive: true,
+                maintainAspectRatio: true,
+                scales: {
+                    xAxes: [{
+                        type: 'time',
+                        time: {
+                            unit: 'minute',
+                            tooltipFormat: "MMM D, h:mm a"
+                        },
+                        bounds: 'data',
+                        ticks: { display: true },
+                        gridLines: {
+                            display: false,
+                            drawBorder: true
+                        }
+                    }],
+                    yAxes: [{     
+                        ticks: { 
+                            display: true,
+                            suggestedMin: 30,
+                            suggestedMax: 80, 
+                        },
+                        gridLines: {
+                            display: true,
+                            drawBorder: false
+                        },
+                    }]
+                }
             }
-        });
+        }); 
     });
 
-    return (<div>
-        <canvas
+    return (
+    <Grid container>
+        <Grid item xs={10}>
+            <div>
+                <canvas
                     id="myChart"
                     ref={chartRef}
                 />
-    </div>)
+            </div>
+        </Grid>
+    </Grid>
+    );
 }
 
 export default Graph;
