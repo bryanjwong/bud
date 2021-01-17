fcc6f18c4b83860da4c4793fd75df0b080e0ed1b
Bryan Wong <bryanjwong@g.ucla.edu>
2021-01-16 19:23:36 -0800

    plant it button turns dark green

--- docs/web/src/Components/Dashboard.js
+++ docs/web/src/Components/Dashboard.js
@@ -69,6 +69,9 @@ const useStyles = makeStyles({
         textTransform: "none",
         borderRadius: 10,
         float: "right",
+        "&:hover": {
+            backgroundColor: "#17402e",
+        },
     }
 });
 
